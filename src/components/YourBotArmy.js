227b210216492900c1ec79adff9ccdb5e6e73de6
Ndindairene <irene.ndindaa@gmail.com>
2023-12-04 22:43:12 +0300

    completed assignment

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -2,23 +2,24 @@ import React from "react";
 import BotCard from "./BotCard";
 
 function YourBotArmy({ bots, removeBot, deleteBot }) {
-  //your bot army code here...
-  const armyItem = bots.map((bot) => {
-    return (
+  // Check if bots is defined and has length before mapping
+  const armyItem = bots && bots.length > 0 ? (
+    bots.map((bot) => (
       <BotCard
         key={bot.id}
         bot={bot}
         clickEvent={removeBot}
         deleteBot={deleteBot}
       />
-    );
-  });
+    ))
+  ) : (
+    <p>No bots in the army.</p>
+  );
 
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
           {armyItem}
         </div>
       </div>
